581bf2b99c45430aa5338d558ff56db6861978fd
valen <vsey@gmx.net>
2026-05-08 10:17:59 +0200

    Switched to basedpyright

--- init.lua
+++ init.lua
@@ -636,7 +636,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
+        basedpyright = {},
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
