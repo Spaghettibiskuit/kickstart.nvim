3f4e29eb3ad7e75ffda5e47add9c6fba37270a9e
valen <vsey@gmx.net>
2026-06-01 18:26:17 +0200

    added spellchecking

--- init.lua
+++ init.lua
@@ -189,6 +189,11 @@ vim.diagnostic.config {
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Spell checking settings
+vim.opt.spell = true
+vim.opt.spelllang = { 'en_us', 'de_20' }
+vim.opt.spelloptions:append 'camel'
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
